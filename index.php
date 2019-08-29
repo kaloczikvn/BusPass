a6bb1dbbe64cee0c80dbcc4295f81e3301eed636
kaloczikvn <kaloczi.kvn@gmail.com>
2019-08-29 18:49:29 +0200

    icon update

--- index.php
+++ index.php
@@ -14,10 +14,19 @@ header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
     <link href="webfonts/font.css" rel="stylesheet">
     <link href="css/style.css?v=4" rel="stylesheet">
     <link rel="manifest" href="manifest.json" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/pwa/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/pwa/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/pwa/favicon-16x16.png">
-    <link rel="mask-icon" href="/pwa/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="57x57" href="img/favico/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="img/favico/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/favico/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/favico/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/favico/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/favico/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/favico/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/favico/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favico/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="img/favico/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="img/favico/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favico/favicon-16x16.png">
     <meta name="msapplication-TileColor" content="#ffc40d">
     <meta name="theme-color" content="#ffc40d">
   </head>
